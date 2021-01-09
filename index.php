70f349833d87d9ab94df475029c8c3491136de94
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-10 03:12:28 +0600

    grabbing title

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Scrapping</title>
+</head>
+<body>
 <?php
+
+include('simple_html_dom.php');
+
+//https://www.marriott.com/reservation/rateListMenu.mi?defaultTab=prepay
+//$html = file_get_html('https://www.google.com');
+$html = file_get_html('https://www.marriott.com/reservation');
+//$html = file_get_html('https://www.marriott.com/reservation/rateListMenu.mi');
+
+
+
+//TITLE
+echo $html->find('title', 0)->plaintext;
+
+?>
+</body>
+</html>
