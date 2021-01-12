d81b377c82d4103477fc638dbf70672c48a6e8dc
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-12 15:28:40 +0600

    getting link and attributes

--- scrape-without-lib.php
+++ scrape-without-lib.php
@@ -16,35 +16,45 @@ $dom = new DOMDocument();
 
 
 
+
+
+
+
+/*
+
 $h3s = $dom->getElementsByTagName('h3');
 
 ////var_dump($h2s);
 //echo "<pre />";
 //print_r($h3s);
 //print_r($h2s[0]->nodeName);
-
-
-
-
 //FINDS ALL H3 TAGS IN PAGE
 echo "<h2 class='ui header blue'> Get all text from h3 tag</h2> <hr />";
-$h2_array = array();
+$h3_array = array();
 foreach($h3s as $h3){
   $title_text = $h3->textContent;
-  $h2_array[] = $title_text;
+  $h3_array[] = $title_text;
   echo "<p class='ui tiny header green'>" .  $title_text . " </p>";
 }
+*/
 
 
 
 //GET ALL LINKS
-echo "<h2 class='ui header blue'> Get all text from h3 tag</h2>  <hr />";
+echo "<h2 class='ui header blue'> Get all links from a tag</h2>  <hr />";
 $articles = $dom->getElementsByTagName('h3');
-$article = $articles->item(0);
+// print_r($articles);
+$article = $articles->item(0);  // GET THE FIRST ELEMENT
+// echo "<pre />";
+// print_r($article);
 $links = $article->getElementsByTagName('a');
 
 //echo "<pre />";
-print_r($links);
+// print_r($links);
+foreach ($links as $link) {
+  // code...
+  echo "<div class='ui segment blue'>" . $link->textContent . " | <p class='ui tiny header teal' > <u> " . $link->getAttribute('href') . " </u> </p> </div>";
+}
 
 //foreach ($links)
 
@@ -55,8 +65,3 @@ print_r($links);
 
 
 ?>
-
-
-
-
-
